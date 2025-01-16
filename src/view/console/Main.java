89646b84840aecebc008507bc6cc3022d09f03cc
Yago García Martínez <yagogarmar@campus.monlau.com>
2025-01-16 15:39:50 +0100

    TEST

--- src/view/console/Main.java
+++ src/view/console/Main.java
@@ -30,32 +30,34 @@ public class Main {
         
         
         // Aeropuertos
-        Aeropuerto barcelona = new Aeropuerto("El Prat", "LEBL", EnumCiudades.BARCELONA);
-        Aeropuerto madrid = new Aeropuerto("Adolfo Suárez Madrid-Barajas", "LEMD", EnumCiudades.MADRID);
-        Aeropuerto sevilla = new Aeropuerto("Sevilla", "LEZL", EnumCiudades.SEVILLA);
-        Aeropuerto paris = new Aeropuerto("Charles de Gaulle", "LFPG", EnumCiudades.PARIS);
-        Aeropuerto roma = new Aeropuerto("Fiumicino", "LIRF", EnumCiudades.ROMA);
-        Aeropuerto berlin = new Aeropuerto("Berlin Brandenburg", "EDDB", EnumCiudades.BERLIN);
-        Aeropuerto colonia = new Aeropuerto("Colonia/Bonn", "EDDK", EnumCiudades.COLONIA);
-        Aeropuerto londres = new Aeropuerto("Heathrow", "EGLL", EnumCiudades.LONDRES);
-        Aeropuerto lisboa = new Aeropuerto("Humberto Delgado", "LPPT", EnumCiudades.LISBOA);
-        Aeropuerto viena = new Aeropuerto("Vienna International", "LOWW", EnumCiudades.VIENA);
-        Aeropuerto bruselas = new Aeropuerto("Brussels Airport", "EBBR", EnumCiudades.BRUSELAS);
-        Aeropuerto atenas = new Aeropuerto("Eleftherios Venizelos", "LGAV", EnumCiudades.ATENAS);
-        Aeropuerto estocolmo = new Aeropuerto("Arlanda", "ESSA", EnumCiudades.ESTOCOLMO);
-        Aeropuerto nuevaYork = new Aeropuerto("John F. Kennedy", "KJFK", EnumCiudades.NUEVA_YORK);
-        Aeropuerto toronto = new Aeropuerto("Toronto Pearson", "CYYZ", EnumCiudades.TORONTO);
-        Aeropuerto monterrey = new Aeropuerto("General Mariano Escobedo", "MMMY", EnumCiudades.MONTERREY);
-        Aeropuerto buenosAires = new Aeropuerto("Ministro Pistarini", "SAEZ", EnumCiudades.BUENOS_AIRES);
-        Aeropuerto lima = new Aeropuerto("Jorge Chávez", "SPJC", EnumCiudades.LIMA);
-        Aeropuerto tokio = new Aeropuerto("Haneda", "RJTT", EnumCiudades.TOKIO);
-        Aeropuerto osaka = new Aeropuerto("Kansai", "RJBB", EnumCiudades.OSAKA);
-        Aeropuerto hongKong = new Aeropuerto("Hong Kong International", "VHHH", EnumCiudades.HONG_KONG);
-        Aeropuerto singapur = new Aeropuerto("Changi", "WSSS", EnumCiudades.SINGAPUR);
-        Aeropuerto estambul = new Aeropuerto("Istanbul", "LTFM", EnumCiudades.ESTAMBUL);
+        ArrayList<Aeropuerto> aeropuertos = new ArrayList<Aeropuerto>();
 
+        aeropuertos.add(new Aeropuerto("El Prat", "LEBL", EnumCiudades.BARCELONA));
+        aeropuertos.add(new Aeropuerto("Adolfo Suárez Madrid-Barajas", "LEMD", EnumCiudades.MADRID));
+        aeropuertos.add(new Aeropuerto("Sevilla", "LEZL", EnumCiudades.SEVILLA));
+        aeropuertos.add(new Aeropuerto("Charles de Gaulle", "LFPG", EnumCiudades.PARIS));
+        aeropuertos.add(new Aeropuerto("Fiumicino", "LIRF", EnumCiudades.ROMA));
+        aeropuertos.add(new Aeropuerto("Berlin Brandenburg", "EDDB", EnumCiudades.BERLIN));
+        aeropuertos.add(new Aeropuerto("Colonia/Bonn", "EDDK", EnumCiudades.COLONIA));
+        aeropuertos.add(new Aeropuerto("Heathrow", "EGLL", EnumCiudades.LONDRES));
+        aeropuertos.add(new Aeropuerto("Humberto Delgado", "LPPT", EnumCiudades.LISBOA));
+        aeropuertos.add(new Aeropuerto("Vienna International", "LOWW", EnumCiudades.VIENA));
+        aeropuertos.add(new Aeropuerto("Brussels Airport", "EBBR", EnumCiudades.BRUSELAS));
+        aeropuertos.add(new Aeropuerto("Eleftherios Venizelos", "LGAV", EnumCiudades.ATENAS));
+        aeropuertos.add(new Aeropuerto("Arlanda", "ESSA", EnumCiudades.ESTOCOLMO));
+        aeropuertos.add(new Aeropuerto("John F. Kennedy", "KJFK", EnumCiudades.NUEVA_YORK));
+        aeropuertos.add(new Aeropuerto("Toronto Pearson", "CYYZ", EnumCiudades.TORONTO));
+        aeropuertos.add(new Aeropuerto("General Mariano Escobedo", "MMMY", EnumCiudades.MONTERREY));
+        aeropuertos.add(new Aeropuerto("Ministro Pistarini", "SAEZ", EnumCiudades.BUENOS_AIRES));
+        aeropuertos.add(new Aeropuerto("Jorge Chávez", "SPJC", EnumCiudades.LIMA));
+        aeropuertos.add(new Aeropuerto("Haneda", "RJTT", EnumCiudades.TOKIO));
+        aeropuertos.add(new Aeropuerto("Kansai", "RJBB", EnumCiudades.OSAKA));
+        aeropuertos.add(new Aeropuerto("Hong Kong International", "VHHH", EnumCiudades.HONG_KONG));
+        aeropuertos.add(new Aeropuerto("Changi", "WSSS", EnumCiudades.SINGAPUR));
+        aeropuertos.add(new Aeropuerto("Istanbul", "LTFM", EnumCiudades.ESTAMBUL));
         
-        Ruta rutaTest = new Ruta(barcelona,madrid);
+        Ruta rutaTest = new Ruta(aeropuertos.get(0),aeropuertos.get(1));
+        
         System.out.println(rutaTest.getDistancia());
         System.out.println(rutaTest.getTiempoDeVuelo());
         
